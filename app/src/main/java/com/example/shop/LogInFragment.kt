617ce191059d8a0c9dc250faad92c338791c80ea
1eG0ist <diadypetya@yandex.ru>
2023-11-01 21:19:37 +0300

    add swap between sign in and sign up

--- app/src/main/java/com/example/shop/LogInFragment.kt
+++ app/src/main/java/com/example/shop/LogInFragment.kt
@@ -5,56 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.shop.databinding.ActivityMainBinding
+import com.example.shop.databinding.FragmentLogInBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LogInFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LogInFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentLogInBinding? = null
+    private val binding: FragmentLogInBinding get() = _binding!!
+    private var stateNow = "in"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false)
+        _binding = FragmentLogInBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LogInFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LogInFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.signInButton.setOnClickListener {
+            if (stateNow == "up") {
+                binding.signInArea.visibility = View.VISIBLE
+                binding.acceptSignInButtonArea.visibility = View.VISIBLE
+                binding.signUpArea.visibility = View.INVISIBLE
+                binding.acceptSignUpButtonArea.visibility = View.INVISIBLE
+                stateNow = "in"
             }
+        }
+        binding.signUpButton.setOnClickListener {
+            if (stateNow == "in") {
+                binding.signInArea.visibility = View.INVISIBLE
+                binding.acceptSignInButtonArea.visibility = View.INVISIBLE
+                binding.signUpArea.visibility = View.VISIBLE
+                binding.acceptSignUpButtonArea.visibility = View.VISIBLE
+                stateNow = "up"
+            }
+        }
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
 }
